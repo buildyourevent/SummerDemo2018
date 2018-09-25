8da4480ee9a927875d6606a8a203a1462f4f6d52
hosk0029 <35551105+hosk0029@users.noreply.github.com>
2018-09-25 13:20:15 -0400

    Update Startup.cs

--- BuildYourEvent/Startup.cs
+++ BuildYourEvent/Startup.cs
@@ -18,9 +18,8 @@ namespace BuildYourEvent
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = @"Server = mssql5.gear.host;Database=byedb1;Uid=XXXXXXX;Pwd=XXXXXXXX;Connection Timeout=30;MultipleActiveResultSets=True";
-
-            //@"Server=localhost\SQLEXPRESS;Database=ByeDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+       
+          
 
             services.AddDbContext<VenuesDataContext>(options => options.UseSqlServer(connection));
             services.AddMvc();
